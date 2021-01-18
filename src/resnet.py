01d42ff3dc2079923e19bcaa0e5a89886a4151db
guillaumeBadikian <guillaume.badikian@gmail.com>
2021-01-18 17:43:17 +0100

    resnet

--- src/resnet.py
+++ src/resnet.py
@@ -1,139 +1,118 @@
+import sns as sns
 import tensorflow as tf
+from cv2 import cv2
 from networkx.drawing.tests.test_pylab import plt
 
 if __name__ == '__main__':
-    #from tensorflow.keras.layers import concatenate
-    #from keras.layers import Dense
-    #from keras.layers import GlobalAveragePooling2D
+    # from tensorflow.keras.layers import concatenate
+    # from keras.layers import Dense
+    # from keras.layers import GlobalAveragePooling2D
     import os
     import pandas as pd
-    #import plotly.graph_objs as go
+
+    # import plotly.graph_objs as go
     # matplotlib.pyplot as plt
     # seaborn as sns
-    #mport cv2.cv2 as cv2
-    #import numpy as np
-    #from sklearn.model_selection import train_test_split
-<<<<<<< HEAD
-    tf.distribute.experimental.TPUStrategy
-=======
-
+    # mport cv2.cv2 as cv2
+    # import numpy as np
+    # from sklearn.model_selection import train_test_split
 
     os.chdir(os.path.dirname(__file__))
-    print(os.path.abspath(__file__))
->>>>>>> 99b060db56138b03710dd7d1b0e6b2ba68432652
     os.chdir(os.getcwd())
-    print(os.path.realpath(__file__))
-    print(os.path.dirname(__file__))
 
     os.listdir('./data/')
-    print("test")
 
-    train=pd.read_csv("data/train.csv")
-    test=pd.read_csv("data/test.csv")
-    train['image_id']=train['image_id']+'.jpg'
-    test['image_id']=test['image_id']+'.jpg'
+    train = pd.read_csv("data/train.csv")
+    test = pd.read_csv("data/test.csv")
+    train['image_id'] = train['image_id'] + '.jpg'
+    test['image_id'] = test['image_id'] + '.jpg'
     train.head()
 
+    fig, ax = plt.subplots(2, 2, figsize=(20, 20))
+    sns.barplot(y=train.healthy.value_counts(), x=train.healthy.value_counts().index, ax=ax[0, 0])
+    ax[0, 0].set_title("Value count for healthy", size=20)
+    ax[0, 0].set_xlabel('healthy', size=18)
+    ax[0, 0].set_ylabel('', size=18)
 
-    fig,ax=plt.subplots(2,2,figsize=(20,20))
-    sns.barplot(y=train.healthy.value_counts(),x=train.healthy.value_counts().index,ax=ax[0,0])
-    ax[0,0].set_title("Value count for healthy",size=20)
-    ax[0,0].set_xlabel('healthy',size=18)
-    ax[0,0].set_ylabel('',size=18)
+    sns.barplot(y=train.multiple_diseases.value_counts(), x=train.multiple_diseases.value_counts().index, ax=ax[0, 1])
+    ax[0, 1].set_title("Value count for multiple_diseases", size=20)
+    ax[0, 1].set_xlabel('multiple_diseases', size=18)
+    ax[0, 1].set_ylabel('', size=18)
 
-    sns.barplot(y=train.multiple_diseases.value_counts(),x=train.multiple_diseases.value_counts().index,ax=ax[0,1])
-    ax[0,1].set_title("Value count for multiple_diseases",size=20)
-    ax[0,1].set_xlabel('multiple_diseases',size=18)
-    ax[0,1].set_ylabel('',size=18)
+    sns.barplot(y=train.rust.value_counts(), x=train.rust.value_counts().index, ax=ax[1, 0])
+    ax[1, 0].set_title("Value count for rust", size=20)
+    ax[1, 0].set_xlabel('rust', size=18)
+    ax[1, 0].set_ylabel('', size=18)
 
-    sns.barplot(y=train.rust.value_counts(),x=train.rust.value_counts().index,ax=ax[1,0])
-    ax[1,0].set_title("Value count for rust",size=20)
-    ax[1,0].set_xlabel('rust',size=18)
-    ax[1,0].set_ylabel('',size=18)
+    sns.barplot(y=train.scab.value_counts(), x=train.scab.value_counts().index, ax=ax[1, 1])
+    ax[1, 1].set_title("Value count for scab", size=20)
+    ax[1, 1].set_xlabel('healthy', size=18)
+    ax[1, 1].set_ylabel('', size=18)
 
-    sns.barplot(y=train.scab.value_counts(),x=train.scab.value_counts().index,ax=ax[1,1])
-    ax[1,1].set_title("Value count for scab",size=20)
-    ax[1,1].set_xlabel('healthy',size=18)
-    ax[1,1].set_ylabel('',size=18)
-
-
-
-    img=[]
-    filename=train.image_id
+    img = []
+    filename = train.image_id
     for file in filename:
-        image=cv2.imread("data/images/"+file)
-        res=cv2.resize(image,(256,256))
+        image = cv2.imread("data/images/" + file)
+        res = cv2.resize(image, (256, 256))
         img.append(res)
-    img=np.array(img)
-
+    img = np.array(img)
 
     print(img.shape)
 
-
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(15, 15))
     for i in range(9):
-        plt.subplot(3,3,i+1)
+        plt.subplot(3, 3, i + 1)
         plt.imshow(img[i])
-
-
-
 
     train_labels = np.float32(train.loc[:, 'healthy':'scab'].values)
 
-    train, val = train_test_split(train, test_size = 0.15)
-
+    train, val = train_test_split(train, test_size=0.15)
 
     from keras.preprocessing.image import ImageDataGenerator
-    train_datagen = ImageDataGenerator( horizontal_flip=True,
-        vertical_flip=True,
-        rotation_range=10,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        zoom_range=.1,
-        fill_mode='nearest',
-        shear_range=0.1,
-        rescale=1/255,
-        brightness_range=[0.5, 1.5])
 
+    train_datagen = ImageDataGenerator(horizontal_flip=True,
+                                       vertical_flip=True,
+                                       rotation_range=10,
+                                       width_shift_range=0.1,
+                                       height_shift_range=0.1,
+                                       zoom_range=.1,
+                                       fill_mode='nearest',
+                                       shear_range=0.1,
+                                       rescale=1 / 255,
+                                       brightness_range=[0.5, 1.5])
 
-    train_generator=train_datagen.flow_from_dataframe(train,directory='./data/images/',
-                                                          target_size=(384,384),
-                                                          x_col="image_id",
-                                                          y_col=['healthy','multiple_diseases','rust','scab'],
-                                                          class_mode='raw',
-                                                          shuffle=False,
-                                                           subset='training',
-                                                          batch_size=32)
+    train_generator = train_datagen.flow_from_dataframe(train, directory='./data/images/',
+                                                        target_size=(384, 384),
+                                                        x_col="image_id",
+                                                        y_col=['healthy', 'multiple_diseases', 'rust', 'scab'],
+                                                        class_mode='raw',
+                                                        shuffle=False,
+                                                        subset='training',
+                                                        batch_size=32)
 
-
-
-    val_generator=train_datagen.flow_from_dataframe(val,directory='./data/images/',
-                                                          target_size=(384,384),
-                                                          x_col="image_id",
-                                                          y_col=['healthy','multiple_diseases','rust','scab'],
-                                                          class_mode='raw',
-                                                          shuffle=False,
-                                                          batch_size=32,
+    val_generator = train_datagen.flow_from_dataframe(val, directory='./data/images/',
+                                                      target_size=(384, 384),
+                                                      x_col="image_id",
+                                                      y_col=['healthy', 'multiple_diseases', 'rust', 'scab'],
+                                                      class_mode='raw',
+                                                      shuffle=False,
+                                                      batch_size=32,
                                                       )
 
-
-
-    test_generator=train_datagen.flow_from_dataframe(test,directory='./data/images/',
-                                                          target_size=(384,384),
-                                                          x_col="image_id",
-                                                          y_col=None,
-                                                          class_mode=None,
-                                                          shuffle=False,
-                                                          batch_size=32)
-
-
-
+    test_generator = train_datagen.flow_from_dataframe(test, directory='./data/images/',
+                                                       target_size=(384, 384),
+                                                       x_col="image_id",
+                                                       y_col=None,
+                                                       class_mode=None,
+                                                       shuffle=False,
+                                                       batch_size=32)
 
     from keras.applications.resnet50 import ResNet50
     from keras.models import Model
     import keras
     from keras import optimizers
-    model_finetuned = ResNet50(include_top=False, weights='imagenet', input_shape=(384,384,3))
+
+    model_finetuned = ResNet50(include_top=False, weights='imagenet', input_shape=(384, 384, 3))
     x = model_finetuned.output
     x = GlobalAveragePooling2D()(x)
     x = Dense(128, activation="relu")(x)
@@ -141,24 +120,18 @@ if __name__ == '__main__':
     predictions = Dense(4, activation="softmax")(x)
     model_finetuned = Model(inputs=model_finetuned.input, outputs=predictions)
     model_finetuned.compile(optimizer='adam',
-                      loss = 'categorical_crossentropy',
-                      metrics=['accuracy'])
+                            loss='categorical_crossentropy',
+                            metrics=['accuracy'])
     model_finetuned.summary()
-
-
-
 
     from keras.callbacks import ReduceLROnPlateau
 
-
     history_1 = model_finetuned.fit_generator(train_generator,
-                                      steps_per_epoch=100,
-                                      epochs=25,validation_data=val_generator,validation_steps=100
-                                      ,verbose=1,callbacks=[ReduceLROnPlateau(monitor='val_loss', factor=0.3,patience=3, min_lr=0.000001)],use_multiprocessing=False,
-                   shuffle=True)
-
-
-
+                                              steps_per_epoch=100,
+                                              epochs=25, validation_data=val_generator, validation_steps=100
+                                              , verbose=1, callbacks=[
+            ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=3, min_lr=0.000001)], use_multiprocessing=False,
+                                              shuffle=True)
 
     fig = go.Figure(data=[
         go.Line(name='train_acc', x=history_1.epoch, y=history_1.history['accuracy']),
@@ -175,7 +148,6 @@ if __name__ == '__main__':
         ))
     fig
 
-
     SUB_PATH = "data/sample_submission.csv"
 
     sub = pd.read_csv(SUB_PATH)
@@ -183,5 +155,3 @@ if __name__ == '__main__':
     sub.loc[:, 'healthy':] = probs_RESNET
     sub.to_csv('submission_RESNET.csv', index=False)
     sub.head()
-
-
